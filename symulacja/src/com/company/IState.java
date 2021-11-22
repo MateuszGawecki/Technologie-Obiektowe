ffc61d94e5fc3700144508c9d9f9fc44151f8c49
MateuszGawecki <72608321+MateuszGawecki@users.noreply.github.com>
2021-11-22 21:47:40 +0100

    symulacja

--- symulacja/src/com/company/IState.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public interface IState {
-    void handle();
-}
--- symulacja/src/com/company/IVector.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public interface IVector {
-    public double abs();
-    public double[] getComponents();
-    public void setComponents(double newX, double newY);
-}
